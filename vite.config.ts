d7a7cc985388fe5a60f929da8af92ded52e3571e
Agzam-Ar <90514725+Agzam-Ar@users.noreply.github.com>
2026-05-17 03:01:57 +0300

    refactor: proxy on backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, HttpProxy, HttpServer, PluginOption, ProxyOptions } from 'vite';
 import preact from '@preact/preset-vite';
 
-export default defineConfig({
-  plugins: [preact()],
-  server: {
-    //    port: 3000,
-    proxy: {
-      '/api': {
+const ponylab = "http://192.168.0.17"
+
+const proxy: Record<string, string | ProxyOptions> = {}
+proxy['/api'] = {
+    target: 'http://localhost:1919',
+    changeOrigin: true,
+}
+proxy['/cmd'] = {
+    target: ponylab
+}
+for (let url of ['/ponylab', "/cfg", "/state"]) {
+    proxy[url] = {
         target: 'http://localhost:1919',
         changeOrigin: true,
-      },
-      '/debugtest': {
-        target: 'http://192.168.0.10',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/debugtest/, '')
-      },
-      '/state': {
-        target: "http://192.168.0.10",
-      },
-      '/cmd': {
-        target: "http://192.168.0.10",
-      },
-      '/cfg': {
-        target: "http://192.168.0.10",
-      }
+    }
+}
+
+export default defineConfig({
+    plugins: [preact()],
+    server: {
+        //    port: 3000,
+        proxy: proxy
     },
-  },
 });
